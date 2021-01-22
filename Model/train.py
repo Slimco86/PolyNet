072fe78efcb14fcecaf8ba39bbdcde5ae626b456
Slimco86 <slimco86@gmail.com>
2021-01-22 22:13:33 +0100

    added validation loop

--- Model/train.py
+++ Model/train.py
@@ -35,7 +35,7 @@ def train(opt):
     train_generator = torch.utils.data.DataLoader(train_data,batch_size=opt.batch_size,shuffle=True,num_workers=8,
                                                     collate_fn=collater,drop_last=True)
 
-    valid_data = AllInOneData(opt.train_path,set='validation',transforms=transforms.Compose([Normalizer(),Resizer()]))
+    valid_data = AllInOneData(opt.train_path,set='test',transforms=transforms.Compose([Normalizer(),Resizer()]))
     valid_generator = torch.utils.data.DataLoader(valid_data,batch_size=opt.batch_size,shuffle=False,num_workers=8,
                                                     collate_fn=collater,drop_last=True)
     
@@ -43,7 +43,7 @@ def train(opt):
     model = EfficientDetMultiBackbone(opt.train_path,compound_coef=0,heads=opt.heads)
     model.to(device)
 
-    min_val_loss = None
+    min_val_loss = 10e5
 
     if opt.optim == 'Adam':
         optimizer = torch.optim.AdamW(model.parameters(),lr=opt.lr)
@@ -56,8 +56,6 @@ def train(opt):
     print(f'Targets are {opt.heads}.')
     verb_loss = 0
     writer = SummaryWriter(logdir=logdir,filename_suffix=f'Train_{"_".join(opt.heads)}',comment='try1')
-    #dummy = torch.ones([1,3,512,512]).cuda()
-    #writer.add_graph(model,dummy)
     
     for epoch in range(opt.epochs):
         model.train()
@@ -92,39 +90,86 @@ def train(opt):
             loss.backward()
             optimizer.step()
             verb_loss = loss.detach().cpu().numpy()
-            writer.add_scalar('Total',verb_loss,epoch)
+            writer.add_scalar('Train/Total',verb_loss,epoch)
             description = f'Epoch:{epoch}| Total Loss:{verb_loss}|'
             for k,v in losses.items():
                 Losses[k]+=v.detach().cpu().numpy()
-                writer.add_scalar(k,v.detach().cpu().numpy(),epoch)
+                writer.add_scalar(f"Train/{k}",v.detach().cpu().numpy(),epoch)
                 description+=f'{k}:{round(np.mean(Losses[k]),1)}|'
             progress_bar.set_description(description)
             
-            if epoch%100==0:
+        if epoch%opt.valid_step==0:
+            im = imgs[0]
+            regressBoxes = BBoxTransform()
+            clipBoxes = ClipBoxes()
+            pp = postprocess(imgs,
+                  out['anchors'], out['person'], out['gender'],
+                  regressBoxes, clipBoxes,
+                  0.4, 0.4)
+            
+            writer.add_image_with_boxes('Train/Box_prediction',im,pp[0]['rois'],epoch)
+            img2 = out['face_landmarks']
+            writer.add_images('Train/landmarks_prediction',img2,epoch)
+            writer.add_images('Train/landmark target', lm_mask,epoch)
+            
+            #VALIDATION STEPS
+            model.eval()
+            valid_Losses = {k:0 for k in opt.heads}
+            
+            val_description = f'Validation| Total Loss:{verb_loss}'
+            progress_bar = tqdm(valid_generator,desc = val_description)
+    
+            for sample in progress_bar:   
+                imgs = sample['img'].to(device)
+                gt_person_bbox = sample['person_bbox'].to(device)
+                gt_face_bbox = sample['face_bbox'].to(device)
+                gt_pose = sample['pose'].to(device)
+                gt_face_landmarks = sample['face_landmarks'].to(device)
+                gt_age = sample['age'].to(device)
+                gt_race = sample['race'].to(device)
+                gt_gender = sample['gender'].to(device)
+                gt_skin = sample['skin'].to(device)
+                gt_emotions = sample['emotion'].to(device)
+                out = model(imgs)
+                annot = {'person':gt_person_bbox,'gender':gt_gender,
+                 'face':gt_face_bbox,'emotions':gt_emotions,
+                 'face_landmarks':gt_face_landmarks,
+                 'pose':gt_pose}
+        
+                losses, lm_mask = criterion(out,annot,out['anchors'])
+        
+                loss = torch.zeros(1).to(device)
+                loss = torch.sum(torch.cat(list(losses.values())))
+                verb_loss = loss.detach().cpu().numpy()
+                writer.add_scalar('Validation/Total',verb_loss,epoch)
+                val_description = f'Validation| Total Loss:{verb_loss}|'
+                for k,v in losses.items():
+                    valid_Losses[k]+=v.detach().cpu().numpy()
+                    writer.add_scalar(f"Validation/{k}",v.detach().cpu().numpy(),epoch)
+                    val_description+=f'{k}:{round(np.mean(valid_Losses[k]),1)}|'
+                progress_bar.set_description(val_description)
                 im = imgs[0]
                 regressBoxes = BBoxTransform()
                 clipBoxes = ClipBoxes()
                 pp = postprocess(imgs,
-                      out['anchors'], out['person'], out['gender'],
-                      regressBoxes, clipBoxes,
-                      0.4, 0.4)
-                
-                writer.add_image_with_boxes('prediction',im,pp[0]['rois'],epoch)
+                  out['anchors'], out['person'], out['gender'],
+                  regressBoxes, clipBoxes,
+                  0.4, 0.4)
+            
+                writer.add_image_with_boxes('Validation/Box_prediction',im,pp[0]['rois'],epoch)
                 img2 = out['face_landmarks']
-                writer.add_images('landmarks prediction',img2,epoch)
+                writer.add_images('Validation/landmarks_prediction',img2,epoch)
+                writer.add_images('Validation/landmark target', lm_mask,epoch)
 
-                #target_map = torch.zeros((opt.batch_size,1,512,512))
-                #target = gt_face_landmarks
-                #target = torch.clamp(target,0,511)
-                #for b in range(opt.batch_size):
-                    #target_map[b,:,target[b,:,:,1].long(),target[b,:,:,0].long()] = 1
-                writer.add_images('landmark target', lm_mask,epoch)
-                #writer.add_image('target image', im,epoch)
+                if verb_loss<min_val_loss:
+                    torch.save(model.state_dict(),f'{logdir}/{opt.save_name.split(".pt")[0]}_best_epoch_{epoch}.pt')
 
+                
 
-            if epoch%1000==0:
-                torch.save(model.state_dict(),f'{logdir}/{opt.save_name.split(".pt")[0]}_epoch_{epoch}.pt')
+        if epoch%1000==0:
+            torch.save(model.state_dict(),f'{logdir}/{opt.save_name.split(".pt")[0]}_epoch_{epoch}.pt')
     torch.save(model.state_dict(),f'{logdir}/{opt.save_name.split(".pt")[0]}_last.pt')
+    writer.close()
         
              
 
@@ -134,9 +179,9 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--train_path',type=str,default='./datasets/Train2021')
     parser.add_argument('--epochs',type=int,default=10000)
-    parser.add_argument('--valid_step',type=int,default=1)
+    parser.add_argument('--valid_step',type=int,default=100)
     parser.add_argument('--lr',type=int,default=1e-3)
-    parser.add_argument('--batch_size',type=int,default=3)
+    parser.add_argument('--batch_size',type=int,default=1)
     parser.add_argument('--momentum',type=int,default=0.9)
     parser.add_argument('--wd',type=int,default=0.8)
     parser.add_argument('--optim',type=str,default='Adam')
